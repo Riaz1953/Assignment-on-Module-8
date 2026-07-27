31425eb962841a8c6350d6c5121821ad36e4c633
Md Riaz <fxkiyan2020@gmail.com>
2026-07-27 20:29:29 +0200

    Assignment finished

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ dotenv.config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Express.js Server" });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).json({ message: "This is About Page" });
+});
+
+app.get("/contact", (req, res) => {
+  res.status(200).json({ message: "This is Contact Page" });
+});
+
+app.get("/services", (req, res) => {
+  res.status(200).json({ message: "Our Services" });
+});
+
+app.get("/students", (req, res) => {
+  res.status(200).json({ message: "Student List" });
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is listening on port : ${process.env.PORT}`);
 });
